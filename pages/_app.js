9fdb332bb6c0bd17e12f9aebd5fea3994fe78233
Mahmoud Salah <mahmoudsalah11350@gmail.com>
2021-12-26 13:48:32 +0200

    Http Request

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import Head from "next/head";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+    <title>Weather App</title>
+    <meta
+      httpEquiv="Content-Security-Policy"
+      content="upgrade-insecure-requests"
+    />
+  </Head>;
   return <Component {...pageProps} />;
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
-import Head from "next/head";
 import Weather from "../components/Weather";
 
 export default function Home() {
-  <Head>
-    <title>Weather App</title>
-    <meta
-      http-equiv="Content-Security-Policy"
-      content="upgrade-insecure-requests"
-    />
-  </Head>;
   return (
     <>
       <Weather />
